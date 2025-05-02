c74c89629a625bf391f401bf05d9acb2e76c91a6
daniel-j-h <527241+daniel-j-h@users.noreply.github.com>
2025-05-02 18:00:49 +0200

    Adds example query

--- index.py
+++ index.py
@@ -19,8 +19,8 @@ class PointHandler(osmium.SimpleHandler):
     def __init__(self):
         super().__init__()
 
-        self.lng = []
-        self.lat = []
+        self.lngs = []
+        self.lats = []
 
     def node(self, n):
         # https://wiki.openstreetmap.org/wiki/Tag:emergency%3Dfire_hydrant
@@ -28,35 +28,54 @@ class PointHandler(osmium.SimpleHandler):
         if "emergency" not in n.tags or n.tags["emergency"] != "fire_hydrant":
             return
 
-        self.lng.append(int((n.location.lon + 180) * 10000000))
-        self.lat.append(int((n.location.lat +  90) * 10000000))
+        self.lngs.append(n.location.lon)
+        self.lats.append(n.location.lat)
 
 
 def main(args):
     handler = PointHandler()
     handler.apply_file(args.input)
 
-    z = [pyzorder.pymorton.interleave(lng, lat)
-         for lng, lat in zip(handler.lng, handler.lat)]
+    zs = []
+    lngs = []
+    lats = []
 
-    # Open question: Should the Z value be of type u64 as in
-    # we interleave the two u32 fixed point lng, lat values
-    # or would a u32 Z value be good enough which translates
-    # to a coarse grid of lng, lat we can query by?
+    for lng, lat in zip(handler.lngs, handler.lats):
+        # 32-bit Z value for a 16-bit x,y-grid
+        lng16 = int((lng + 180) * ((2**16 - 1) / 360))
+        lat16 = int((lat +  90) * ((2**16 - 1) / 180))
+        assert 0 <= lng16 <= 2**16-1
+        assert 0 <= lat16 <= 2**16-1
+
+        z32 = pyzorder.pymorton.interleave(lng16, lat16)
+        assert 0 <= z32 <= 2**32-1
+
+        zs.append(z32)
+
+        # 32-bit unsigned fixed representation
+        lng32 = int((lng + 180) * 10000000)
+        lat32 = int((lat +  90) * 10000000)
+        assert 0 <= lng32 <= 2**32-1
+        assert 0 <= lat32 <= 2**32-1
+
+        lngs.append(lng32)
+        lats.append(lat32)
 
     schema = pyarrow.schema([
-        pyarrow.field("z", pyarrow.uint64(), nullable=False),
+        pyarrow.field("z", pyarrow.uint32(), nullable=False),
         pyarrow.field("lng", pyarrow.uint32(), nullable=False),
         pyarrow.field("lat", pyarrow.uint32(), nullable=False),
     ])
 
-    table = pyarrow.Table.from_arrays([z, handler.lng, handler.lat], schema=schema)
+    table = pyarrow.Table.from_arrays([zs, lngs, lats], schema=schema)
     table = table.sort_by("z")
 
     # Open question: Do we need to fine-tune the row group
     # size and/or the page size below for our use case some
     # more? We want to support rather local bounding box
     # queries translating into a few linear range queries.
+    #
+    # Open question: What about column encoding, compression
 
     pyarrow.parquet.write_table(table, args.out,
         sorting_columns=[pyarrow.parquet.SortingColumn(0)],
--- query.py
+++ query.py
@@ -5,47 +5,126 @@ import argparse
 from pathlib import Path
 
 # todo: add proper dependency management
-# pip install duckdb
+# pip install duckdb pyzorder
 
 import duckdb
 
+import pyzorder
+
 
 def main(args):
-    # Below is an example query but what we really need to do is
-    # take a query bounding box of [lngmin, latmin, lngmax, latmax]
-    # and turn that into [zmin, zmax] and then prune it with the
-    # LITMAX/BIGMIN optimization technique. Then below we can
-    # query by multiple z ranges. It's not obvious to me if it's
-    # beneficial to prune down to the exact z ranges or if there
-    # is a trade off where we prune to rough z ranges and add an
-    # additional predicate on the bounding box as in
-    #
-    #   WHERE (z BETWEEN lo AND hi
-    #          AND lng BETWEEN lngmin AND lngmax
-    #          AND lat BETWEEN latmin AND latmax)
-    #
-    # to reduce the number of queries we need to make in total.
-    #
-    # This is similar to the optimization in zbush to only compute
-    # BIGMIN and jump to it if the range of irrelevant data is large:
-    #
-    # https://github.com/daniel-j-h/zbush/blob/1bed328cc2788cd516d3fca7b0a546f6c27d9983/index.js#L144-L171
+    # See index.py: Z values are 32-bit on a 16-bit x,y-grid
+    # in the parquet column. The full 32-bit lng, lat are
+    # available in the lng, lat columns to use.
 
-    points = duckdb.execute("""
-        select lng, lat from 'berlin-latest-hydrants.parquet' where
-          z between 3973762480109379858 and 3973762480196724606
+    xmin16 = int((args.lngmin + 180) * ((2**16 - 1) / 360))
+    xmax16 = int((args.lngmax + 180) * ((2**16 - 1) / 360))
+    assert 0 <= xmin16 <= 2**16-1
+    assert 0 <= xmax16 <= 2**16-1
+
+    ymin16 = int((args.latmin +  90) * ((2**16 - 1) / 180))
+    ymax16 = int((args.latmax +  90) * ((2**16 - 1) / 180))
+    assert 0 <= ymin16 <= 2**16-1
+    assert 0 <= ymax16 <= 2**16-1
+
+    assert xmin16 <= xmax16
+    assert ymin16 <= ymax16
+
+    zmin32 = pyzorder.pymorton.interleave(xmin16, ymin16)
+    zmax32 = pyzorder.pymorton.interleave(xmax16, ymax16)
+    assert 0 <= zmin32 <= 2**32-1
+    assert 0 <= zmax32 <= 2**32-1
+
+    assert zmin32 <= zmax32
+
+    # We now have a range of [zmin, zmax] we know all our
+    # points are within. We can now prune this range by
+    # irrelevant data ranges due to Z discontinuities
+    # using the BIGMIN optimization implemented in the
+    # pyzorder packages as next_zorder_index(z) below.
+
+    zindex = pyzorder.ZOrderIndexer((xmin16, xmax16), (ymin16, ymax16))
+
+    zs = []
+
+    while zmin32 < zmax32:
+        zs.append(zmin32)
+        zmin32 = zindex.next_zorder_index(zmin32)
+
+    zs.append(zmax32)
+
+    # The list zs now contains all Z values pruned by
+    # irrelevant data ranges, meaning our points are
+    # exactly in there. We now have multiple options:
+    #
+    # 1. Do a single SQL query with a WHERE IN operator
+    #    and list all values in zs. Pro: Single query
+    #    Con: Will most likely cause troubles when
+    #    the list of Z values gets big.
+    #
+    # 2. Split the list into sub-ranges and make multiple
+    #    SQL queries with WHERE z BETWEEN lo and hi. Pro:
+    #    We can run multiple queries potentially in parallel.
+    #    Con: It might create many small queries when points
+    #    are sparse and the query window is large.
+    #    Example for three sub-queries querying ranges on z:
+    #    [1001, 1003], [1007, 1009], [4095, 4096]
+    #
+    # 3. Split the list into sub-ranges and make multiple
+    #    SQL queries with WHERE z BETWEEN lo and hi but
+    #    allow the sub-ranges to be non-contiguous and
+    #    add in the WHERE filter in addition
+    #      AND lng BETWEEN lngmin AND lngmax
+    #      AND lat BETWEEN latmin AND latmax
+    #    Pro: This allows the query engine to make use of
+    #    the sorting on z to pre-filter very efficiently
+    #    and then filter the candidate points by lng, lat
+    #    exactly. It's the best solution of the three
+    #    allowing us to tune how many sub-queries to run
+    #    vs. how much exact filtering the query engine
+    #    will have to do. Same example from above,
+    #    but allowing non-contiguous ranges:
+    #    [1001, 1009], [4095, 4096]. Note how the ranges
+    #    [1001, 1003] and [1007, 1009] get merged into one
+    #    and the precise filtering by lng, lat will take
+    #    care of the rest.
+    #
+    # All three approaches are very efficient since they
+    # always filter by the z column (WHERE z ...) which
+    # benefits from the z sorting in addition to the row
+    # group and data page information in the parquet file.
+
+    lngmin32 = int((args.lngmin + 180) * 10000000)
+    lngmax32 = int((args.lngmax + 180) * 10000000)
+    latmin32 = int((args.latmin + 90) * 10000000)
+    latmax32 = int((args.latmax + 90) * 10000000)
+
+    points = duckdb.execute(f"""
+        select
+          lng, lat
+        from
+          'berlin-latest-hydrants.parquet'
+        where
+          z in ({','.join(map(str, zs))})
+          and lng between {lngmin32} and {lngmax32}
+          and lat between {latmin32} and {latmax32};
         """).fetchall()
 
-    for lng, lat in points:
-        lng = round(lng / 10000000 - 180, 6)
-        lat = round(lat / 10000000 - 90, 6)
+    for lng32, lat32 in points:
+        assert lngmin32 <= lng32 <= lngmax32
+        assert latmin32 <= lat32 <= latmax32
+
+        lng = round(lng32 / 10000000 - 180, 6)
+        lat = round(lat32 / 10000000 - 90, 6)
 
         print(f"point: {lng}, {lat}", file=sys.stderr)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument("input", type=Path, help=".parquet")
+    parser.add_argument("--lngmin", type=float, default=13.4083)
+    parser.add_argument("--latmin", type=float, default=52.5217)
+    parser.add_argument("--lngmax", type=float, default=13.4115)
+    parser.add_argument("--latmax", type=float, default=52.5234)
 
     main(parser.parse_args())
